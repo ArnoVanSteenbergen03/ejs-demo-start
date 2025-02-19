5ff8946674f199f5eaf9ffe46761f1ad73204a12
ArnoVanSteenbergen03 <arno.vansteenbergen@student.arteveldehs.be>
2025-02-19 13:51:52 +0100

    odg

---
+++ src/controllers/pagecontroller.js
@@ -0,0 +1,40 @@
+import { person } from "../data/data.js";
+import { navItems } from "../data/data.js";
+
+export const home = (req, res) => {
+  // res.render("<NAAM VAN DE VIEW>", {<DATA NAAR DE VIEW>})
+  res.render("pages/home", {
+    title: "Dinosaurs around the world",
+    content: "Welcome to the world of dinosaurs",
+    navItems,
+  });
+};
+export const about = (req, res) => {
+  res.render("pages/default", {
+    title: "About us",
+    content: "We are a group of dinosaur enthusiasts",
+    team: [
+      "T-Rex",
+      "Velociraptor",
+      "Stegosaurus",
+      "<strong>Premium</strong> Diplodocus",
+    ],
+    navItems,
+  });
+};
+export const contact = (req, res) => {
+  res.render("pages/contact", {
+    title: "Contact",
+    content: "Contact us at 09 233 40 89",
+    person,
+    navItems,
+  });
+};
+export const privacy = (req, res) => {
+  res.render("pages/default", {
+    title: "Privacy Policy",
+    content: "Dinosaurs are protective of their privacy",
+    team: false,
+    navItems,
+  });
+};
